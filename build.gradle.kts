18c82dd0a85e3b7af4be465ab243008a0222acdf
Christoph Welcz <christoph.welcz@codecentric.de>
2024-04-11 17:19:16 +0200

    ♻️ reorder handler helper functions to make sense 📝 add http demo requests

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-  implementation("io.arrow-kt:arrow-core:1.2.1")
+  implementation("io.arrow-kt:arrow-core:1.2.2")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.projectreactor:reactor-test")
--- src/main/kotlin/de/welcz/hipstertdd/helpers/HandlerExt.kt
+++ src/main/kotlin/de/welcz/hipstertdd/helpers/HandlerExt.kt
@@ -11,19 +11,19 @@ import org.springframework.web.reactive.function.server.queryParamOrNull
 suspend fun <T> Either<RequestError, T>.foldServerResponse(ifRight: suspend (T) -> ServerResponse) =
   fold({ it.responseError() }, { ifRight(it) })
 
-fun ServerRequest.extractNumberFromPath(pathVariable: String) = either {
-  val input = pathVariable(pathVariable).toIntOrNull()
-  ensureNotNull(input) { InvalidNumber(pathVariable) }
-}
-
 suspend fun Any.responseOk() = ServerResponse.ok().bodyValueAndAwait(this)
 
 suspend fun RequestError.responseError() = when (this) {
   is InvalidNumber -> ServerResponse.badRequest().bodyValueAndAwait(this)
 }
 
-data class InvalidNumber(val pathVariable: String) : RequestError("not a valid number")
 sealed class RequestError(@Suppress("unused") val message: String)
+data class InvalidNumber(val pathVariable: String) : RequestError("not a valid number")
+
+fun ServerRequest.extractNumberFromPath(pathVariable: String) = either {
+  val input = pathVariable(pathVariable).toIntOrNull()
+  ensureNotNull(input) { InvalidNumber(pathVariable) }
+}
 
 fun ServerRequest.extractNumberFromQuery(queryVariable: String, defaultValue: Int) = either {
   val limit = queryParamOrNull(queryVariable) ?: return@either defaultValue
